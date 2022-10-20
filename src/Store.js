6ee00dfdeb912c49b3c1e54d9769637df6514508
prince-unk <105361236+prince-unk@users.noreply.github.com>
2022-10-20 23:00:15 +0530

    update

--- src/Store.js
+++ src/Store.js
@@ -4,4 +4,4 @@ import rootReducer from './reducer/index';
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-export default store
+export default store;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addTodo } from '../actions/index';
-import todoReducer from '../reducer/Todoreducer.js';
+// import todoReducer from '../reducer/Todoreducer.js';
 
 
 
